d7df8ac3f5a017a06156f1215591f327e17269ee
Phethiso11 <phethisomasenyetse@gmail.com>
2024-08-21 12:38:20 +0200

    edited the files

--- crypto-tracker/.next/static/chunks/webpack.js
+++ crypto-tracker/.next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1a2d8410796052d7"; }
+/******/ 		__webpack_require__.h = function() { return "15f01e301fb3d319"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- crypto-tracker/pages/index.js
+++ crypto-tracker/pages/index.js
@@ -89,12 +89,12 @@ const CryptoCard = ({ crypto }) => {
     if (!crypto) return null;
 
     return (
-        <div style={styles.cryptoCard}>
+        <div>
             <h2>{crypto.name} ({crypto.symbol.toUpperCase()})</h2>
-            <p>Price: ${crypto.price?.usd}</p>
-            <p>Market Cap: ${crypto.market_cap?.usd}</p>
-            <p>24h Change: {crypto.price?.usd_24h_change}%</p>
-            <p>Volume: ${crypto.volume?.usd}</p>
+            <p>Price: ${crypto.price.usd}</p>
+            <p>Market Cap: ${crypto.market_cap.usd}</p>
+            <p>24h Change: {crypto.price.usd_24h_change}%</p>
+            <p>Volume: ${crypto.volume.usd}</p>
         </div>
     );
 };
